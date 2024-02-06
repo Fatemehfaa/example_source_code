edc1b359dd4db677b8446a1c15dee0c2272c3a10
mrtechhunter <mahdihamldar@gmail.com>
2024-02-06 15:45:04 +0330

    fatemeh neveshte yek khat

---
+++ fatemeh/src/singlton/Person.java
@@ -0,0 +1,14 @@
+package singlton;
+
+import java.util.ArrayList;
+
+public class Person {
+    ArrayList<Person> person = new ArrayList<>();
+
+    private Person() {
+    }
+
+    private static 
+
+
+}
